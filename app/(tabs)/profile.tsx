85c41c52f121c1767d2b00f27429a2a7c2f13bff
Alva Ringbom <alva.ringbom@chasacademy.se>
2025-06-10 10:49:59 +0200

    profile tab

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from "expo-router";
+import { signOut } from "firebase/auth";
+import { Button, StyleSheet, Text, View } from "react-native";
+import auth from "../../firebase/firebase";
+
+const Profile = () => {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      console.log("Navigera till login");
+      router.replace("/login");
+    } catch (error) {
+      console.error("Fel vid utloggning:", error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Detta är profilsidan</Text>
+      <Button title="Logga ut" onPress={handleLogout} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+});
+
+export default Profile;
